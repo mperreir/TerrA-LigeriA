4a7832636e185e1174aca89545242504019c68b4
Houssem <houssem-eddine.kacemi@etu.univ-nantes.fr>
2022-02-23 00:13:45 +0100

    Premier essai de detection de mouvement

--- museum-botanique/groupe-4/src/test.js
+++ museum-botanique/groupe-4/src/test.js
@@ -1,3 +1,4 @@
+
 const data = {
     "MHNN.B.010101.009":{
         "Collection":"BOURGAULT DUCOUDRAY",
@@ -20,12 +21,86 @@ const data = {
         "PartieConserve":"Inflorescence",
         "Rarete":"",
         "Image": "../data/mhnn.b.10101.013.jpg"
+    },
+    "MHNN.B.010101.038":{
+        "Collection":"BOURGAULT DUCOUDRAY",
+        "DateEntree:":1877,
+        "LieuCollecte":"Nantes",
+        "DateCollecte":"00.04.1848",
+        "NomScientifique":"Anemone stellata Lam. Anemone hortensis L. var. stellata (Lam.) Gren. & Godr.",
+        "Famille":"Ranunculaceae",
+        "PartieConserve":"InflorescenceFeuille",
+        "Rarete":"",
+        "Image": "../data/mhnn.b.10101.038.jpg"
     }
+}
+
+const seuilMouvement = 1000;
+const tempsAttente = 80;
+const values = Object.values(data)
+var index = 0;
+var doitAttendre = false;
+var compteur = 0;
+
+const controller = new Leap.Controller();
+controller.loop(function(frame) {
+    if(!doitAttendre){
+    if (frame.hands[0]){
+      //console.log(frame.hands[0].palmVelocity);
+      //haveLoggedFrame = true;
+      const actionRealisee = gererMouvement(frame.hands[0]);
+      if (actionRealisee) {
+          doitAttendre = true;
+          compteur = 0;
+      }
+    }
+}
+    else {
+        compteur++;
+        if (compteur >= tempsAttente){
+            doitAttendre = false;
+        }
+    }
+
+  });
+
+
+function gererMouvement(hand){
+    const xVelocity = hand.palmVelocity[0];
+    const yVelocity = hand.palmVelocity[1];
+
+    //console.log(hand.palmVelocity);
+    if ((Math.abs(xVelocity) >= seuilMouvement) && Math.abs(xVelocity) >= Math.abs(yVelocity)){
+        if (xVelocity>0)
+            defilerVersDroite();
+        else
+            defilerVersGauche();
+        return true;
+        
+    }
+    else if (Math.abs(yVelocity) >= seuilMouvement){
+        if (yVelocity>0)
+            console.log("Détails");
+        
+        return true;
+    }
+    return false;
+}
+
+function defilerVersGauche(){
+    console.log("défiler gauche");
+    index = Math.abs((index - 1) % values.length);
+    image.src = values[index].Image
+}
+
+function defilerVersDroite(){
+    console.log("défiler droite");
+    index = (index + 1) % values.length;
+    image.src = values[index].Image
 }
 
 bouton = document.getElementById("BT")
 image = document.getElementById("img")
-values = Object.values(data)
 i = 0
 bouton.addEventListener("click", (evt) => {
     i = (i+1)%values.length
