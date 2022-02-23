a75c275335fbaefeaa837a2c82594ced568b0719
Alexis Courteille <alexis.courteille@etu.univ-nantes.fr>
2022-02-23 10:13:37 +0100

    Integrate socket.io

--- museum-botanique/groupe-1/web/app.js
+++ museum-botanique/groupe-1/web/app.js
@@ -7,10 +7,17 @@ const https = require('https');
 const key = fs.readFileSync('./certs/local.key');
 const cert = fs.readFileSync('./certs/local.crt');
 
+const { IOServer } = require("socket.io");
+
 const port = 443;
+
 
 app.use(express.static('public'));
 
-const server = https.createServer({key: key, cert: cert }, app);
+const server = https.createServer({ key: key, cert: cert }, app);
+
+const io = new IOServer(server);
+
+io.on('connection', (socket) => { console.log('a user connected'); });
 
 server.listen(port, () => { console.log(`listening on ${port}`) });
