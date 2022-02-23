167696f94c63794ae97aaf4189dfe998c82f3b84
Antoine ELUERE <antoine.eluere1@etu.univ-nantes.fr>
2022-02-23 12:41:34 +0100

    Séparation des data dans un autre fichier

--- museum-botanique/groupe-4/src/test.js
+++ museum-botanique/groupe-4/src/test.js
@@ -1,6 +1,38 @@
-import {data_json} from "./data.js";
-
-const data = data_json
+const data = {
+    "MHNN.B.010101.009":{
+        "Collection":"BOURGAULT DUCOUDRAY",
+        "DateEntree:":1977,
+        "LieuCollecte":"Nantes",
+        "DateCollecte":"1851",
+        "NomScientifique":"Clematis smilacifolia Wall.",
+        "Famille":"Ranunculaceae",
+        "PartieConserve":"HerbierFeuille",
+        "Rarete":"",
+        "Image": "../data/mhnn.b.10101.009.jpg"
+    },
+    "MHNN.B.010101.013":{
+        "Collection":"BOURGAULT DUCOUDRAY",
+        "DateEntree:":1977,
+        "LieuCollecte":"Nantes",
+        "DateCollecte":"00.07.184",
+        "NomScientifique":"Clematis bicolor Steud",
+        "Famille":"Ranunculaceae",
+        "PartieConserve":"Inflorescence",
+        "Rarete":"",
+        "Image": "../data/mhnn.b.10101.013.jpg"
+    },
+    "MHNN.B.010101.038":{
+        "Collection":"BOURGAULT DUCOUDRAY",
+        "DateEntree:":1877,
+        "LieuCollecte":"Nantes",
+        "DateCollecte":"00.04.1848",
+        "NomScientifique":"Anemone stellata Lam. Anemone hortensis L. var. stellata (Lam.) Gren. & Godr.",
+        "Famille":"Ranunculaceae",
+        "PartieConserve":"InflorescenceFeuille",
+        "Rarete":"",
+        "Image": "../data/mhnn.b.10101.038.jpg"
+    }
+}
 const seuilMouvement = 1000;
 const tempsAttente = 60;
 const values = Object.values(data);
