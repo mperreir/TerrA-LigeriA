32c54a11c4e38ed56258611119ba861b335e9b93
Alexis Courteille <alexis.courteille@etu.univ-nantes.fr>
2022-02-23 10:08:03 +0100

    Update to HTTPS

--- museum-botanique/groupe-1/web/app.js
+++ museum-botanique/groupe-1/web/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
-const port = 8080;
+const https = require('https');
+
+const key = fs.readFileSync('./certs/local.key');
+const cert = fs.readFileSync('./certs/local.crt');
+
+const port = 443;
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
-    console.log(`Botaweb listening on port ${port}`);
-})
+const server = https.createServer({key: key, cert: cert }, app);
+
+server.listen(port, () => { console.log(`listening on ${port}`) });
