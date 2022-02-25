f1916cdfbaf4c39cd2fb50f08c6aa6d6f9bb8b37
Houssem <houssem-eddine.kacemi@etu.univ-nantes.fr>
2022-02-25 09:33:50 +0100

    correction bug vue vivant

--- museum-botanique/groupe-4/src/vivant.js
+++ museum-botanique/groupe-4/src/vivant.js
@@ -10,4 +10,34 @@ const swiper = new Swiper('.swiper-container', {
     loop: true,
     fadeEffect: {
         crossFade: true
-    }})
+}})
+
+let currentPage = 1
+
+    
+document.addEventListener('slideLeft', (evt) => {
+    console.log("left");
+    swiper.slidePrev(1250);
+    if (currentPage > 1){
+        $('.flipped')
+            .last()
+            .toggleClass('flipped active')
+            .siblings('.page')
+            .removeClass('active');
+        currentPage--;
+    }
+})
+    
+document.addEventListener('slideRight', (evt) => {
+    console.log("right");
+    swiper.slideNext(1250);
+    if (currentPage < 3){
+        $('.active')
+            .toggleClass('active flipped')
+            .next('.page')
+            .addClass('active');
+        currentPage++;
+    }
+})
+    
+    
