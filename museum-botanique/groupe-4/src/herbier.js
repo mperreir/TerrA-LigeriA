df95d215d6d0d01072f68ae3fc9f94263710dda3
iheb <iheb66@yahoo.com>
2022-02-25 10:48:59 +0100

    changement de l'animation slide vertical

--- museum-botanique/groupe-4/src/herbier.js
+++ museum-botanique/groupe-4/src/herbier.js
@@ -1,22 +1,5 @@
 let currentPage = 1
 
-
-const options = {
-    keepalive: 60,
-    clientId: 1,
-    protocolId: 'MQTT',
-    protocolVersion: 4,
-    clean: true,
-    reconnectPeriod: 1000,
-    connectTimeout: 30 * 1000,
-    will: {
-        topic: 'WillMsg',
-        payload: 'Connection Closed abnormally..!',
-        qos: 0,
-        retain: false
-    },
-}
-
 document.addEventListener('slideLeft', (evt) => {
     if (currentPage > 1){
         $('.flipped')
@@ -40,31 +23,46 @@ document.addEventListener('slideRight', (evt) => {
 })
 
 document.addEventListener('slideUp', (evt) => {
-    // speed of animations (ms)
-    let speed = 250;
-    // non active slides moved down so they can slide up when activated
-    $(".slide[pos!='" + active_slide + "']").each(function() {
-        $(this).css("top", "10px");
-    })
+    console.log("details")
+    changeSlide()
+    // // speed of animations (ms)
+    // let speed = 250;
+    // // non active slides moved down so they can slide up when activated
+    // $(".slide[pos!='" + active_slide + "']").each(function() {
+    //     $(this).css("top", "10px");
+    // })
 
-    if (active_slide === 1) {
+    // if (active_slide === 1) {
 
-        /*
-          Note: delay only works if .hide() or .show() are in its internal queue. Therefore you need to pass an argument to it, even if it's 0. (praise be to stackoverflow)
-        */
+    //     /*
+    //       Note: delay only works if .hide() or .show() are in its internal queue. Therefore you need to pass an argument to it, even if it's 0. (praise be to stackoverflow)
+    //     */
 
-        $(".slide[pos='" + active_slide + "']").animate({opacity:0, top: "-10px"}, {duration: speed}).hide(0).animate({top: "10px"});
-        active_slide = 2;
-        var slide = $(".slide[pos='" + active_slide + "']");
-        slide.delay(speed).show(0).animate({opacity:1, top: "0px"}, {duration: speed});
-        slide.html("")
-        slide.append(generate_table(currentPage -1))
+    //     $(".slide[pos='" + active_slide + "']").animate({opacity:0, top: "-10px"}, {duration: speed}).hide(0).animate({top: "10px"});
+    //     active_slide = 2;
+    //     var slide = $(".slide[pos='" + active_slide + "']");
+    //     slide.delay(speed).show(0).animate({opacity:1, top: "0px"}, {duration: speed});
+    //     slide.html("")
+    //     slide.append(generate_table(currentPage -1))
 
-    } else {
-        $(".slide[pos='" + active_slide + "']").animate({opacity:0, top: "-10px"}, {duration: speed}).hide(0).animate({top: "10px"});
-        active_slide = 1;
-        $(".slide[pos='" + active_slide + "']").delay(speed).show(0).animate({opacity: 1, top: "0px"});
-    }
+    // } else {
+    //     $(".slide[pos='" + active_slide + "']").animate({opacity:0, top: "-10px"}, {duration: speed}).hide(0).animate({top: "10px"});
+    //     active_slide = 1;
+    //     $(".slide[pos='" + active_slide + "']").delay(speed).show(0).animate({opacity: 1, top: "0px"});
+    // }
 
 })
 
+const changeSlide = () => {
+    const sliderContainer = document.querySelector(".slider-container");
+    const slidesLength = 2;
+    const sliderHeight = sliderContainer.clientHeight;
+    console.log(slidesLength)
+    active_slide++;
+    if (active_slide > slidesLength - 1) {
+        active_slide = 0;
+    }
+    sliderContainer.style.transform = `translateY(-${
+        active_slide * sliderHeight
+    }px)`;
+  };
