3410b05a95fc4fc0277ee812607e0475a2921f1b
Zuber <antoinezuber19@gmail.com>
2022-03-02 17:52:47 +0100

    Ajout des dernieres planches de données

---
+++ museum-botanique/groupe-4/src/js/herbier.js
@@ -0,0 +1,53 @@
+let currentPage = 1
+var configObject = {
+    autoSave: false,
+    autoLoad: false,
+    layers: ["scene"]
+};
+var maptastic = Maptastic(configObject);
+
+document.addEventListener('slideLeft', (evt) => {
+    if (currentPage > 1){
+        $('.flipped')
+            .last()
+            .toggleClass('flipped active')
+            .siblings('.page')
+            .removeClass('active');
+        currentPage--;
+    }
+})
+
+document.addEventListener('slideRight', (evt) => {
+    window.postMessage("Prev","*")
+    if (currentPage < 5){
+        $('.active')
+            .toggleClass('active flipped')
+            .next('.page')
+            .addClass('active');
+        currentPage++;
+    }
+    else{
+        currentPage = 1;
+        retourDebut();
+    }
+})
+
+document.addEventListener('slideUp', (evt) => {
+    $(`.details[slide="${active_slide}"]`).addClass("animate");
+})
+
+function retourDebut(){
+    console.log("here");
+    const first = $(".page").first()[0].cloneNode(true);
+    $(".book")[0].appendChild(first);
+    $(".page").last().toggleClass('active flipped')
+    .next('.page')
+    .addClass('active');
+    $(".page").first().toggleClass('active flipped');
+    $(".book div:not(:first)").toggleClass("flipped");
+    first.remove();
+    
+}
+document.addEventListener('slideDown', (evt) => {
+    $(`.details[slide="${active_slide}"]`).removeClass("animate")
+})
---
+++ museum-botanique/groupe-4/src/js/loop.js
@@ -0,0 +1,98 @@
+const seuilMouvementHorizontal = 500;
+const seuilMouvementVertical = 1300
+const tempsAttente = 75;
+var doitAttendre = false;
+var compteur = 0;
+var active_slide = 1;
+var tps_inactif
+
+const evtLeft = new CustomEvent('slideLeft')
+const evtRight = new CustomEvent('slideRight')
+const evtUp = new CustomEvent('slideUp')
+const evtDown = new CustomEvent('slideDown')
+
+
+const controller = new Leap.Controller();
+controller.loop(function(frame) {
+    const d = new Date();
+
+    if(!doitAttendre){
+        if (frame.hands[0]){
+            const actionRealisee = gererMouvement(frame.hands[0]);
+            if (actionRealisee) {
+                doitAttendre = true;
+                compteur = 0;
+            }
+        }
+    }
+
+    else {
+        compteur++;
+        if (compteur >= tempsAttente){
+            doitAttendre = false;
+        }
+    }
+
+    //Compteur pour reset 
+    if (frame.hands.length > 0) {
+        tps_inactif = d.getTime();
+    }
+    if (d.getTime() - tps_inactif > 20 * 1000) {
+        history.go(0)
+    }
+});
+
+
+
+function gererMouvement(hand){
+    const xVelocity = hand.palmVelocity[0];
+    const yVelocity = hand.palmVelocity[1];
+
+    if ((Math.abs(xVelocity) >= seuilMouvementHorizontal) && (Math.abs(xVelocity) / seuilMouvementHorizontal ) >= ( Math.abs(yVelocity) / seuilMouvementVertical )){
+        if (active_slide === 2)
+            document.dispatchEvent(evtUp);
+        else if (xVelocity>0)
+            document.dispatchEvent(evtLeft);
+        else
+            document.dispatchEvent(evtRight);
+        return true;
+    }
+    else if (Math.abs(yVelocity) >= seuilMouvementVertical){
+        document.dispatchEvent(evtUp);
+        return true;
+    }
+    return false;
+}
+document.onkeydown = e => {
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+            if (currentPage != 1) { 
+            //up arrow
+            active_slide = active_slide + 1;
+            if (active_slide >= 3) active_slide = 3
+            document.dispatchEvent(evtUp);
+        }
+    }
+    else if (e.keyCode == '40') {
+        if (currentPage != 1) {
+            //down arrow
+            document.dispatchEvent(evtDown);
+            active_slide = active_slide - 1;
+            if (active_slide <= 1) active_slide = 1
+        }
+    }
+    else if (e.keyCode == '37') {
+       //left arrow
+       if (active_slide == 1){
+        document.dispatchEvent(evtLeft);
+       }
+    }
+    else if (e.keyCode == '39') {
+       //right arrow
+       if (active_slide == 1){
+        document.dispatchEvent(evtRight);
+
+       }
+    }    
+}
