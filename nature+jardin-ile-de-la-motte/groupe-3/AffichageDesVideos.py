cea4ed8a9dbd281b605895f56c3c29cca45122ee
Quentin Santerne <e20a411a@etu.univ-nantes.fre20a411a@etu.univ-nantes.fr>
2022-03-06 16:02:19 +0100

    ajout de commentaires pour vidéos et sons

--- nature+jardin-ile-de-la-motte/groupe-3/AffichageDesVideos.py
+++ nature+jardin-ile-de-la-motte/groupe-3/AffichageDesVideos.py
@@ -2,8 +2,12 @@ import numpy as np
 import cv2 as cv
 import socket
 
+# Initialisation des vidéos
+
+# Vidéo qui tourne lorsqu'il n'y à pas de plaque
 plateau = cv.VideoCapture("./Videos/Plateau.mp4")
 
+# Vidéos plaque été
 e0 = cv.VideoCapture("./Videos/Estivale.mp4")
 e1 = cv.VideoCapture("./Videos/Aigrette_garzette.mp4")
 e2 = cv.VideoCapture("./Videos/Estivale.mp4") # Rien à ajouter plus tard
@@ -11,6 +15,7 @@ e3 = cv.VideoCapture("./Videos/Phragmite_des_joncs.mp4")
 e3bis = cv.VideoCapture("./Videos/Rousserolle_affarvatte.mp4")
 e4 = cv.VideoCapture("./Videos/Estivale.mp4") # Rien à ajouter plus tard
 
+# Vidéos plaque hiver
 h0 = cv.VideoCapture("./Videos/Hivernale.mp4")
 h1 = cv.VideoCapture("./Videos/Hivernale.mp4") # Rien à ajouter plus tard
 h2 = cv.VideoCapture("./Videos/Becassine_des_marais.mp4")
@@ -18,19 +23,20 @@ h3 = cv.VideoCapture("./Videos/Hivernale.mp4") # Rien à ajouter plus tard
 h4 = cv.VideoCapture("./Videos/Sarcelle_hiver.mp4")
 h4bis = cv.VideoCapture("./Videos/Courlis_cendre.mp4")
 
+# booléen pour alterner entre les vidéos double sur les même zones (bis)
 val_e3 = True
 val_h4 = True
 
+# Initialisation des la fenêtre en pleine écran pour afficher les vidéos
 cv.namedWindow("window", cv.cv2.WND_PROP_FULLSCREEN)
 cv.setWindowProperty("window",cv.WND_PROP_FULLSCREEN,cv.WINDOW_FULLSCREEN)
 
+# Initialisation de la première image à afficher
 vid = plateau
 ret, frame = vid.read()
 frame = cv.rotate(frame, cv.ROTATE_90_COUNTERCLOCKWISE)
 
-dim = frame.shape
-blackscreen = np.zeros((dim[1],dim[0],3), np.uint8)
-
+# Initialisation des variables liées au socket
 zone = 0
 saison = "R"
 
@@ -39,7 +45,7 @@ PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    print("attente de msg")
+    print("attente de msg du socket")
     while True :
         s.listen()
         conn, addr = s.accept()
@@ -47,19 +53,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         with conn:
             print("Connected by", addr)
             while True:
-                data = conn.recv(1024)
+                data = conn.recv(1024) # Bloaquant, en attente d'un message du socket
                 if not data:
                     break
                 data = data.decode('UTF-8')
 
+                # Si le socket envoie un données liée à un changement de plaque
                 if data in ['E','H','R']:
                     saison = data
                 else:
-                    zone = int(data)
+                    zone = int(data)    # Ou a une de la main au dessus d'une zone
                 print(data)
                 
                 i = 0
 
+                # 'Choix' de la vidéo à afficher en fonction des données reçues
                 if saison == "E":
                     if zone == 1:
                         vid = e1
@@ -96,13 +104,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     vid = plateau
                     zone = 0
 
-                while i <= 36:
+                # Affichage d'une boucle complète de vidéo
+                while i <= 39:
                     ret, frame = vid.read()
                     frame = cv.rotate(frame, cv.ROTATE_90_COUNTERCLOCKWISE)    
                     cv.imshow("window", frame)
                     i += 1
                     if cv.waitKey(1) & 0xFF == ord('q'):
                         exit()
+                
+                # Puis remise à la frame 0 de la vidéo
                 vid.set(1,0)
         
 
--- nature+jardin-ile-de-la-motte/groupe-3/managesounds.py
+++ nature+jardin-ile-de-la-motte/groupe-3/managesounds.py
@@ -1,31 +1,36 @@
 from pygame import mixer
 import pygame
 import socket
-import time
-
-HOST = '127.0.0.1'
-PORT = 65432
 
 
-# Starting the mixer
-# Starting the mixer
+# Démarrage du mixer
 mixer.init()
-# Loading the song
 
+# Initialisation des sons
+
+# Sons de l'été
 sound=pygame.mixer.Sound("Sons/A.1.0.wav")
 sound1=pygame.mixer.Sound("Sons/A.1.1.wav")
 sound2=pygame.mixer.Sound("Sons/A.1.2.wav")
 sound3=pygame.mixer.Sound("Sons/A.1.3.wav")
 sound4=pygame.mixer.Sound("Sons/A.1.4.wav")
+
+# Sons de l'hiver
 sound5=pygame.mixer.Sound("Sons/B.2.0.wav")
 sound6=pygame.mixer.Sound("Sons/B.2.1.wav")
 sound7=pygame.mixer.Sound("Sons/B.2.2.wav")
 sound8=pygame.mixer.Sound("Sons/B.2.3.wav")
 sound9=pygame.mixer.Sound("Sons/B.2.4.wav")
 
-saison= ""
+
+# Initialisation des variables liées au socket
+HOST = '127.0.0.1'
+PORT = 65432
+
+saison= "R"
 zone= 0
 
+# Un channel est une piste sur laquelle pygame va lancer un son
 channel1 = pygame.mixer.Channel(0)
 
 
@@ -39,24 +44,26 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         with conn:
             print("Connected by", addr)
             while True:
-                data = conn.recv(1024)
+                data = conn.recv(1024) # Bloaquant, en attente d'un message du socket
                 if not data:
                     break
                 data = data.decode('UTF-8')
 
-                print(data)
+                # Si le socket envoie un données liée à un changement de plaque
                 if data in ['E','H','R']:
                     saison = data
                 else:
-                    zone = int(data)
-
+                    zone = int(data)    # Ou a une de la main au dessus d'une zone
+                print(data)
+                
+                # 'Choix' du son à diffuser en fonction des données reçues
                 if saison == "E" :
 
                     if zone == 1:
-                        channel1.fadeout(500)
-                        while channel1.get_busy():
+                        channel1.fadeout(500)   # On diminue le volume du son en cours
+                        while channel1.get_busy():  # On attend que le son se termine (que le channel ne soit plus occupé)
                              pass
-                        channel1.play(sound,loops=-1,fade_ms=500)
+                        channel1.play(sound,loops=-1,fade_ms=500)   # On lance le nouveau son
 
                     elif zone == 2: 
                         channel1.fadeout(500)
@@ -113,8 +120,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         while channel1.get_busy():
                              pass
                         channel1.play(sound9,loops=-1,fade_ms=500)
+                # S'il n'y a plus de plaque, on diminue puis coupe le son
                 else:
-                    channel1.fadeout(500)
+                    channel1.fadeout(500)       
                     while channel1.get_busy():
                         pass
 
