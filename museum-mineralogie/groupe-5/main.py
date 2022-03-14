586b7236c439a609c3fb221c7aff4819eb08c42e
stvinho <96258063+stvinho@users.noreply.github.com>
2022-03-14 18:49:58 +0100

    Update main.py

--- museum-mineralogie/groupe-5/main.py
+++ museum-mineralogie/groupe-5/main.py
@@ -3,8 +3,8 @@ import subprocess
 
 if __name__ == '__main__':
 
-    t1 = Thread(target=subprocess.run, args=(["venv/Scripts/python", "./src/python/ardui.py"],))
-    t2 = Thread(target=subprocess.run, args=(["venv/Scripts/python", "./src/python/spectro.py"],))
+    t1 = Thread(target=subprocess.run, args=(["env/Scripts/python", "./src/python/ardui.py"],))
+    t2 = Thread(target=subprocess.run, args=(["env/Scripts/python", "./src/python/spectro.py"],))
 
     t1.daemon = True;
     t2.daemon = True;
